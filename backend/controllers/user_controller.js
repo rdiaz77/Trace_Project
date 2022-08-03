26185e5235d958b321f34f1e576cd8bfc9e8726f
RDM_ <rafadiazm77@gmail.com>
2022-08-03 16:10:51 -0500

    backend almost working fine w/o put and post routes working

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -32,7 +32,52 @@ user.get('/:id', async (req, res) => {
     }
 })
 
+//ADD A NEW USER -- not working
 
+
+user.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body)
+        res.status(200).json({
+            message: 'user created Successfully',
+            data: newUser
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE A USER -- not working
+user.put('/:id', async (req, res) => {
+    try {
+        const updatedUser = await User.update(req.body, {
+            where: {
+                user_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedUser} band(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE A USER -- working
+user.delete('/:id', async (req, res) => {
+    try {
+        const deletedUser = await User.destroy({
+            where: {
+                user_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedUser} user(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 
 
