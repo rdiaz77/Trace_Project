56bf10918f275bdb6d78a45c582ce299c931995d
RDM_ <rafadiazm77@gmail.com>
2022-08-03 19:37:14 -0500

    navbar added

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,3 +2,38 @@ import React from 'react';
 
 
 
+function Navbar(){
+
+    return <nav className='nav'>
+        <a href='/' className='site-title'>SVTrace</a>
+
+            <ul>
+                <li>
+                    <a href="/dashboard">Dashboard</a>
+                </li>
+                <li>
+                    <a href='/users'>Users</a>
+                </li>
+                <li>
+                    <a href='/map'>Map</a>
+                </li>
+                <li>
+                    <a href='/contacts'>Contacts</a>
+                </li>
+                <li>
+                    <a href='/places'>Users</a>
+                </li>
+                <li>
+                    <a href='/credentials'>Credentials</a>
+                </li>
+              
+
+                
+            </ul>
+
+        </nav>
+    
+}
+
+
+export default Navbar
