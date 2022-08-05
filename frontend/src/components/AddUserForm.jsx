05f5868115ec1e9466894fd2754a9b6d9911a4ec
RDM_ <rafadiazm77@gmail.com>
2022-08-05 17:42:09 -0500

    fetch for user form is connected

--- frontend/src/components/AddUserForm.jsx
+++ frontend/src/components/AddUserForm.jsx
@@ -19,21 +19,20 @@ export default function AddUserForm(){
 
 	};
 
-	async function handleSubmit (e) {
-	console.log(user)
-		e.preventDefault();
+	async function handleSubmit(e) {
+		e.preventDefault()
 		const db = 'http://localhost:3000/users'
-    	const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ user })
-    };
-    await fetch(db, requestOptions)
-        .then(response => console.log('Submitted successfully'))
-        .catch(error => console.log('Form submit error', error))
-  };
+
+		await fetch(db, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(user)
+		})
+
 		
-	
+	}
 
 	
 	return(
