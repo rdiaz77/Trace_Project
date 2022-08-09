2c5a3b49fe15a7ea0ebb265f175a0c333a5e6d05
RDM_ <rafadiazm77@gmail.com>
2022-08-08 20:09:36 -0500

    added dashboard

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DashboardCard from '../components/DashboardCard';
+import { Typography, Container, Box} from '@mui/material';
 
 
 
@@ -9,10 +10,45 @@ export default function Dashboard(){
 
     return(
         <div>
-           <h1>dashboard page</h1>
-           <DashboardCard />
-           <DashboardCard />
-           <DashboardCard />
+            <div>
+                <Typography variant='h4'>Dashboard Page</Typography>
+            </div>
+
+           <Container maxWidth="lg">
+                <Box sx={{
+                        p: 1,
+                        m: 1,
+                        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : 'grey.100'),
+                        color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
+                        border: '1px solid',
+                        borderColor: (theme) =>
+                        theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+                        borderRadius: 2,
+                        fontSize: '0.875rem',
+                        fontWeight: '700',
+                    }}>
+                    <Box sx={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', p: 1, m: 1, bgcolor: 'background.paper',borderRadius: 1,}}>
+
+                        <Box sx={{ bgcolor: '#cfe8fc' }}>
+                            <DashboardCard />
+                        </Box>
+                        <Box sx={{ bgcolor: '#cfe8fc' }} >
+                            <DashboardCard />
+
+                        </Box>
+                        <Box sx={{ bgcolor: '#cfe8fc' }}>
+                            <DashboardCard />
+
+                        </Box>
+                        <Box sx={{ bgcolor: '#cfe8fc' }}>
+                            <DashboardCard />
+
+                        </Box>
+                        
+                    </Box>
+                </Box>
+
+           </Container>
 
         </div>
     )
